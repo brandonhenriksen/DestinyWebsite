8cf95bf53f644492f6bab7b4c6db0b7815f04ad4
brandonhenriksen <dunerideryz@gmail.com>
2014-08-13 13:53:35 -0500

    Added Website Files to Directory

---
+++ footer.php
@@ -0,0 +1,38 @@
+<!-- footer
+  ================================================== -->
+<footer>
+
+    <div class="row">
+
+        <div class="twelve columns">
+
+            <ul class="copyright">
+                <li>&copy; Copyright 2014 Zoon</li>
+                <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#intro"><i class="icon-up-open"></i></a></div>
+
+</footer> <!-- Footer End-->
+
+<!-- Java Script
+================================================== -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+<script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+<script src="js/gmaps.js"></script>
+<script src="js/waypoints.js"></script>
+<script src="js/jquery.countdown.js"></script>
+<script src="js/jquery.placeholder.js"></script>
+<script src="js/backstretch.js"></script>
+<script src="js/init.js"></script>
+
+</body>
+
+</html>
