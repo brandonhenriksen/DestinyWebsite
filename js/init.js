eceeeab8e0d618152fee0f740b7f04aea685d0e9
brandonhenriksen <dunerideryz@gmail.com>
2014-08-17 08:03:53 -0500

    Updated Sign Up Page

--- js/init.js
+++ js/init.js
@@ -91,7 +91,7 @@ jQuery(document).ready(function($) {
    setTimeout( function() {
 
 	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	   
+
    }, 100);
 
 /*----------------------------------------------------*/
--- signup.php
+++ signup.php
@@ -4,6 +4,10 @@
 include 'header.php';
 include "database.php";
 
+$gamertagErr = "";
+$gamertag = "";
+
+
 if(!empty($_POST['Gamertag']))
 {
     $exists = $db->query("SELECT * FROM `user` WHERE name = '" . $_POST['Gamertag'] . "'");
@@ -14,9 +18,30 @@ if(!empty($_POST['Gamertag']))
     {
         $db->exec("insert into USER VALUES('". $_POST['Gamertag'] ."') ");
     }else{
-        echo "<p class='error'>Gamertag already exists!</p>";
+        $gamertagErr = "* PSN ID already exists!";
+    }
+
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(empty($_POST['Gamertag'])){
+        $gamertagErr = "* PSN ID is required";
+    }else{
+        $gamertag = test_input($_POST["Gamertag"]);
     }
 }
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+
+
+
+
 
 ?>
 
@@ -24,10 +49,24 @@ if(!empty($_POST['Gamertag']))
     <div id = "mc_embed_signup">
 
         <form action="signup.php" method="post">
-            <label class="gamertagLabel" for="Gamertag">Enter Gamertag:</label>
-            <div>
-                <input type = "text" id = "Gamertag" name="Gamertag" class ="gamertagSubmit">
+            <div class ="submitForm">
+                <h1>Sign Up</h1>
+                <label class="sFormLabel" for="Gamertag">
+                   <span>PSN ID:</span><input type = "text" id = "Gamertag" name="Gamertag" class ="gamertagSubmit">
+                    <label class="errorLabel"> <?php echo $gamertagErr;?></label>
+                    <br/>
+                </label>
+
+
+
+
+
+
+
+
             </div>
+
+
 
 
         </form>
