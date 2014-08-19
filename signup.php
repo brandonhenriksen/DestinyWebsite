0e6148336f6e7565decb485f2b3e2dd99a9dee9c
brandonhenriksen <dunerideryz@gmail.com>
2014-08-19 01:23:51 -0500

    Updated Sign Up Page

--- signup.php
+++ signup.php
@@ -36,38 +36,78 @@ function test_input($data) {
 ?>
 
 <div class="container moon-background">
-    <div class="row ">
+    <div class="row">
 
-    <div class="columns small-12 text-center">
+    <div class="columns small-12">
 
-        <div class ="intro">
+        <div class ="intro text-center">
 
-            <?php if(isset($thanks)): ?>
+<!--            --><?php //if(isset($thanks)): ?>
 
                 <h1>Sign Up</h1>
 
+
+
+
                 <form action="signup.php" method="POST">
 
-                    <div class ="input-wrapper">
+                    <div class ="row">
 
                         <label class="sFormLabel error" for="Gamertag">PSN ID:
-                            <input type = "text" id = "Gamertag" name="Gamertag" class ="gamertagSubmit" autofocus>
-                            <?php if(isset($gamertagErr)): ?><small class="error"><?php echo $gamertagErr;?></small><?php endif; ?>
-                        </label>
-
-                        <div class="large-12 columns">
-                            <button type="submit" class="medium button green">Submit</button>
-                        </div>
-
+                            </label>
                     </div>
 
+                    <div class ="row">
+                        <div class="large-12 columns">
+                            <label class="sFormLabel error" for="Gamertag">
+                                <input type = "text" id = "Gamertag" name="Gamertag" class ="gamertagSubmit" autofocus>
+                                <?php if(isset($gamertagErr)): ?><small class="error"><?php echo $gamertagErr;?></small><?php endif; ?>
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class ="row">
+                        <div class="large-12 columns">
+                            <label class="sFormLabel error">Availability:</label>
+                        </div>
+                    </div>
+
+                    <div class ="row">
+                        <div class="large-6 columns">
+                            <label class="sFormLabel error">Days Available
+                                <select>
+                                    <option value="monday">Monday</option>
+                                    <option value="tuesday">Tuesday</option>
+                                    <option value="wednesday">Wednesday</option>
+                                    <option value="thursday">Thursday</option>
+                                    <option value="friday">Friday</option>
+                                    <option value="saturday">Saturday</option>
+                                    <option value="sunday">Sunday</option>
+                                </select>
+                            </label>
+                        </div>
+
+                        <div class="large-6 columns">
+                            <label class="sFormLabel error">Times Available
+                                <select>
+                                    <option DISABLED>Placeholder for Slider</option>
+
+                                </select>
+                            </label>
+                        </div>
+                        
+                    </div>
+
+                    <div class="large-12 columns">
+                        <button type="submit" class="button radius roundButton">Submit</button>
+                    </div>
                 </form>
 
-            <?php elseif(isset($saved) && $saved): ?>
+<!--            --><?php //elseif(isset($saved) && $saved): ?>
 
-                <h2>Thanks!</h2>
+<!--                <h2>Thanks!</h2>-->
 
-            <?php endif; ?>
+<!--            --><?php //endif; ?>
 
         </div>
 
