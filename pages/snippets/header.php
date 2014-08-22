91a89ed2a4e7efadf30085d21a0323d35ea81bad
Logan Henson <logan@loganhenson.com>
2014-08-21 19:39:12 -0500

    fixed header nav url

--- pages/snippets/header.php
+++ pages/snippets/header.php
@@ -40,7 +40,7 @@
 
     <ul id="nav" class="nav">
         <li><a class="smoothscroll" href="../../index.php">Home</a></li>
-        <li><a class="smoothscroll" href="signup.php">Sign Up</a></li>
+        <li><a class="smoothscroll" href="<?php echo $app->router->urlFor('signup'); ?>">Sign Up</a></li>
 
 
     </ul> <!-- end #nav -->
